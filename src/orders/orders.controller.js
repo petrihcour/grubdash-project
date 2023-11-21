0af7f99436293cf95f459dbaabf0d611441f473f
Vanessa <zdperfectx@gmail.com>
2023-11-21 13:27:57 -0600

    created middleware for dishquantity property being valid

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -8,18 +8,57 @@ const nextId = require("../utils/nextId");
 
 // TODO: Implement the /orders handlers needed to make the tests pass
 
-
 // GET, List orders
 function list(req, res, next) {
-    res.json({ data: orders });
+  res.json({ data: orders });
 }
 
 // MIDDLEWARE STATUS CODE 400 & error message validation for:
-// deliverTo property is missing or empty "Order must include a deliverTo"
-// mobileNumber property is missing	or empty "Order must include a mobileNumber"
-// DISHES property is missing "Order must include a dish"
+// deliverTo, mobileNumber, DISHES property is missing or empty "Order must include a ..."
+function bodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
+    }
+    next({
+      status: 400,
+      message: `Order must include a ${propertyName}`,
+    });
+  };
+}
+
 // DISHES property is not an array or is empty "Order must include at least one dish"
+function dishPropertyIsValid(req, res, next) {
+  const { data: { dishes } = {} } = req.body;
+  if (!Array.isArray(dishes) || dishes.length === 0) {
+    return next({
+      status: 400,
+      message: `Order must include at lease one dish`,
+    });
+  }
+  next();
+}
+
 // a dish quantity property is missing, is 0 or less, or is not an integer	"Dish ${index} must have a quantity that is an integer greater than 0"
+function dishQuantityPropertyIsValid(req, res, next) {
+  const { dishes } = res.locals;
+
+  for (let index = 0; index < dishes.length; index++) {
+    const dish = dishes[index];
+    if (
+      !dish.quantity ||
+      dish.quantity <= 0 ||
+      !Number.isInteger(dish.quantity)
+    ) {
+      return next({
+        status: 400,
+        message: `Dish ${index} must have a quantity that is an integer greater than 0`,
+      });
+    }
+  }
+  next();
+}
 
 // MIDDLEWARE if no matching order of id w/ specified orderId is found, return 404 "Order id does not match route id. Order: ${id}, Route: ${orderId}."
 
@@ -27,18 +66,17 @@ function list(req, res, next) {
 
 // MIDDLWARE FOR STATUS property of the existing order === "delivered"	"A delivered order cannot be changed"
 
-
 // POST, Create an order. SAVES order and responds with NEWLY CREATED ORDER. What status code is that?????
 // Use 'nextId' function to assign a new id
 
 // GET, Read specific order by id (MIDDLEWARE FOR NO MATCHING ORDER)
 
-// PUT, Update specific order id (MIDDLEWARE FOR NO MATCHING ORDER) * MUST INCLUDE SAME VALIDATION AS POST /ORDERS, AND MIDDLEWARE FOR STATUS PROPERTY MISSING OR EMPTY, & STATUS PROPERTY OF EXISTING ORDER === DELIVERED 
+// PUT, Update specific order id (MIDDLEWARE FOR NO MATCHING ORDER) * MUST INCLUDE SAME VALIDATION AS POST /ORDERS, AND MIDDLEWARE FOR STATUS PROPERTY MISSING OR EMPTY, & STATUS PROPERTY OF EXISTING ORDER === DELIVERED
 
 // MIDDLEWARE VALIDATION for STATUS property of the order !== "pending", "An order cannot be deleted unless it is pending. Returns a 400 status code"
 
 // DELETE, Destroy specific order id, return 204 message where id === :orderId and NO RESPONSE BODY, or return 404 if no matching order found. USE VALIDATION FOR ORDER !== PENDING
 
 module.exports = {
-    list,
-}
+  list,
+};
